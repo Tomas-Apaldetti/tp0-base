da63fbfc7ea4f4d56c1fb93ceba10c8194d65d19
Tomas-L-Apal <tapaldetti@fi.uba.ar>
2024-09-02 18:22:49 -0300

    test

--- generar-compose.sh
+++ generar-compose.sh
@@ -10,12 +10,6 @@ fi
 NUM_CLIENTS=$2
 OUTPUT_FILE=$1
 
-# Validate that NUM_CLIENTS is a positive integer
-if ! [[ "$NUM_CLIENTS" =~ ^[0-9]+$ ]] || [ "$NUM_CLIENTS" -le 0 ]; then
-    echo "Error: The number of clients must be a positive integer."
-    exit 1
-fi
-
 # Start building the Docker Compose content
 cat <<EOF > "$OUTPUT_FILE"
 version: '3.8'
