a10cf8a5d0670879af36da94be83183361a532ba
Tomas-L-Apal <tapaldetti@fi.uba.ar>
2024-09-02 20:21:54 -0300

    run docker

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,24 +1,6 @@
-NETWORK_INSPECT=$(docker network inspect tp0_testing_net)
-: ${SERVER_IP:=$(echo "$NETWORK_INSPECT" | awk -F'"' '/"Name": "server"/ {getline; getline; getline; print $4}' | cut -d'/' -f1)}
-: ${SERVER_PORT:=12345}
-
-# Check if SERVER_IP is set
-if [ -z "$SERVER_IP" ]; then
-  echo "Error: SERVER_IP environment variable is not set."
-  exit 1
-fi
-
-# Check if SERVER_PORT is set
-if [ -z "$SERVER_PORT" ]; then
-  echo "Error: SERVER_PORT environment variable is not set."
-  exit 1
-fi
-
-
 TEST_MESSAGE="Probando"
 
-RESPONSE=$(echo "Probando" | nc $SERVER_IP $SERVER_PORT)
-
+RESPONSE=$(docker run --rm --name test --network tp0_testing_net alpine:latest sh -c 'apk add --no-cache netcat-openbsd > /dev/null && echo "Probando" | nc -w 1 server 12345')
 if [ "$RESPONSE" = "$TEST_MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
